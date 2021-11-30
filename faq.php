6eb3645bb9c18acf7d8e1dcaa4f7a43c6326db6f
Stefano De Capitani <stefano.deca@live.com>
2021-11-30 18:56:00 +0100

    Creative recursive function getDeepContent

--- faq.php
+++ faq.php
@@ -44,9 +44,12 @@ $tab_content = [
         "tag" => "p",
         "attributes" => [],
         "children" => [
-            "text" => "decisione della Corte di giustizia dell'Unione europea",
-            "tag" => "a",
-            "href" => "#"
+            0 => [
+                "text" => "decisione della Corte di giustizia dell'Unione europea",
+                "tag" => "a",
+                "attributes" => ["href" => "#"],
+                "children" => []
+            ]
         ]
     ],
     [
@@ -60,10 +63,13 @@ $tab_content = [
         "tag" => "p",
         "attributes" => [],
         "children" => [
-            "text" => "modulo web",
-            "tag" => "a",
-            "attributes" => ["href" => "#"],
-            "children" => []
+            0 => [
+                "text" => "modulo web",
+                "tag" => "a",
+                "attributes" => ["href" => "#"],
+                "children" => []
+            ]
+            
         ]
     ],
     [
@@ -101,20 +107,25 @@ $tab_content = [
         "tag" => "p",
         "attributes" => [],
         "children" => [
-            "text" => "Centro Google per la sicurezza online",
-            "tag" => "a",
-            "attributes" => ["href" => "#"],
-            "children" => []
+            0 => [
+                "text" => "Centro Google per la sicurezza online",
+                "tag" => "a",
+                "attributes" => ["href" => "#"],
+                "children" => []
+            ]
         ]
     ],
     [
         "text" => "# quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
         "tag" => "p",
+        "attributes" => [],
         "children" => [
-            "text" => "Scopri",
-            "tag" => "a",
-            "attributes" => ["href" => "#"],
-            "children" => []
+            0 => [
+                "text" => "Scopri",
+                "tag" => "a",
+                "attributes" => ["href" => "#"],
+                "children" => []
+            ]
         ]
     ],
     [
@@ -131,47 +142,48 @@ $tab_content = [
         ]
     ],
     [
-        "text" => "# ##",
+        "text" => "# #",
         "tag" => "ol",
         "attributes" => [],
         "children" => [
-            [
+            0 => [
                 "text" => "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
                 "tag" => "li",
                 "attributes" => [],
                 "children" => [
-                    "text" => "# ##",
-                    "tag" => "ul",
-                    "attributes" => [],
-                    "children" => [
-                        [
-                            "text" => "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
-                            "tag" => "li",
-                            "attributes" => [],
-                            "children" => []
-                        ],
-                        [
-                            "text" => "Google LLC, con sede negli Stati Uniti, per il resto del mondo..",
-                            "tag" => "li",
-                            "attributes" => [],
-                            "children" => []
-                        ],
+                    0 => ["text" => "# #",
+                        "tag" => "ul",
+                        "attributes" => [],
+                        "children" => [
+                            0 => [
+                                "text" => "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
+                                "tag" => "li",
+                                "attributes" => [],
+                                "children" => []
+                            ],
+                            1 => [
+                                "text" => "Google LLC, con sede negli Stati Uniti, per il resto del mondo.",
+                                "tag" => "li",
+                                "attributes" => [],
+                                "children" => []
+                            ],
+                        ]
                     ]
                 ]
             ],
-            [
+            1 => [
                 "text" => "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
                 "tag" => "li",
                 "attributes" => [],
                 "children" => []
             ]
-        ],
-        [
-            "text" => "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
-            "tag" => "p",
-            "attributes" => [],
-            "children" => []
-        ],
+        ],  
+    ],
+    [
+        "text" => "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
+        "tag" => "p",
+        "attributes" => [],
+        "children" => []
     ],
     [
         "text" => "Stabilire il paese associato al tuo account",
@@ -196,10 +208,10 @@ $tab_content = [
         "tag" => "p",
         "attributes" => [],
         "children" => [
-            [
+            0 => [
                 "text" => "Contattaci",
                 "tag" => "a",
-                "attributes" => [],
+                "attributes" => ["href" => "#"],
                 "children" => []
             ]
         ]
@@ -211,23 +223,23 @@ $tab_content = [
         "children" => []
     ],
     [
-        "text" => "II risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi # del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, ##. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile ###.",
+        "text" => "II risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi # del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, #. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile #.",
         "tag" => "p",
         "attributes" => [],
         "children" => [
-            [
+            0 => [
                 "text" => "contattare il webmaster",
                 "tag" => "a",
                 "attributes" => ["href" => "#"],
                 "children" => []
             ],
-            [
+            1 => [
                 "text" => "fai click qui",
                 "tag" => "a",
                 "attributes" => ["href" => "#"],
                 "children" => []
             ],
-            [
+            2 => [
                 "text" => "visitare la nostra pagina di assistenza per avere ulteriori informazioni",
                 "tag" => "a",
                 "attributes" => ["href" => "#"],
@@ -242,17 +254,17 @@ $tab_content = [
         "children" => []
     ],
     [
-        "text" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di #. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili ##. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
+        "text" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di #. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili #. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
         "tag" => "p",
         "attributes" => [],
         "children" => [
-            [
+            0 => [
                 "text" => "URL referrer",
                 "tag" => "a",
                 "attributes" => ["href" => "#"],
                 "children" => []
             ],
-            [
+            1 => [
                 "text" => "qui",
                 "tag" => "a",
                 "attributes" => ["href" => "#"],
@@ -260,7 +272,85 @@ $tab_content = [
             ],
         ]
     ],
-]
+];
+
+function getDeepContent($text, $children){
+    $content = "";
+    if(count($children) === 0) {
+        return $text;
+    } else {
+        $substrings = explode("#", $text);
+
+        foreach($children as $id => $child){
+            $content .= $substrings[$id];
+
+            $child_text = $child["text"];
+            $tag = $child["tag"];
+            $attributes = $child["attributes"];
+            $grandchildren = $child['children'];
+
+            $content .= "<$tag ";
+            if($attributes){
+                foreach($attributes as $attr => $value){
+                    $content .= "$attr='$value'";
+                }
+            }
+            $content .= " >";
+
+            $content .= getDeepContent($child_text, $grandchildren);
+            $content .= "</$tag>";
+        } 
+
+        if(count($children) < count($substrings)){
+            $content .= end($substrings);
+        }
+    }
+    return $content;
+};
+
+function getSuperDeepContent($elem){
+    $text = $elem["text"];
+    $tag = $elem["tag"];
+    $attributes = $elem["attributes"];
+    $children = $elem["children"];
+    $content = "";
+
+    $content .= "<$tag " . getHTMLAttributes($attributes) . " >"; 
+
+    if(count($children) === 0) {
+        $content .= $text;
+    } else {
+        $content .= substitutePlaceholdersWithChildrensContent($text, "#", $children);
+    }
+    $content .= "</$tag>";
+    return $content;
+}
+
+function getHTMLAttributes($attributes){
+    $HTML = "";
+    if($attributes){
+        foreach($attributes as $attr => $value){
+            $HTML .= "$attr='$value'";
+        }
+    }
+    return $HTML;
+}
+
+function substitutePlaceholdersWithChildrensContent($text, $placeholder, $children){
+    $substrings = explode($placeholder, $text);
+    $content = "";
+
+    foreach($children as $id => $child){
+        $content .= $substrings[$id];
+        $content .= getSuperDeepContent($child);
+    } 
+    if(count($children) < count($substrings)){
+        $content .= end($substrings);
+    }
+
+    return $content;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -296,19 +386,24 @@ $tab_content = [
     <main>
         <?php
             foreach($tab_content as $elem){
+               /*  echo getSuperDeepContent($elem); */
+
                 $text = $elem["text"];
                 $tag = $elem["tag"];
                 $attributes = $elem["attributes"];
                 $children = $elem["children"];
 
                 echo "<$tag ";
-                if(count($attributes) != 0){
+                if($attributes){
                     foreach($attributes as $attr => $value){
                         echo "$attr='$value'";
                     }
                 }
                 echo " >";
-                echo $text;
+
+                $content = getDeepContent($text, $children);
+                echo $content;
+
                 echo "</$tag>";
             }
         ?>
